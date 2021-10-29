c4aeaff1f5245d0821bd0d006f635785b1df2f1f
Travis ROberts <tsrob50@gmail.com>
2021-10-29 09:50:31 -0500

    Remove steps now GA

--- 1_Register-Features.ps1
+++ 1_Register-Features.ps1
@@ -10,15 +10,6 @@ Get-AzContext
 # Set subscription 
 Set-AzContext
 
-<# No longer needed, now GA
-# Register Features
-# Register the Virtual Machine Template Preview feature
-Register-AzProviderFeature -ProviderNamespace Microsoft.VirtualMachineImages -FeatureName VirtualMachineTemplatePreview
-# Do not continue until the feature changes from Registering to Registered
-# Verify status with this command:
-Get-AzProviderFeature -ProviderNamespace Microsoft.VirtualMachineImages -FeatureName VirtualMachineTemplatePreview
-#>
-
 # Register resource providers if not already registered
 Get-AzResourceProvider -ProviderNamespace Microsoft.Compute, Microsoft.KeyVault, Microsoft.Storage, Microsoft.VirtualMachineImages |
   Where-Object RegistrationState -ne Registered |
